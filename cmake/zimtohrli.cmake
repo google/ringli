98636409b2c7f39914c2f0b604316de8a06e48af
Martin Bruse <zond@google.com>
2024-04-22 07:58:03 +0000

    Bumped Zimtohrli version.

--- cmake/zimtohrli.cmake
+++ cmake/zimtohrli.cmake
@@ -1,7 +1,7 @@
 FetchContent_Declare(zimtohrli
     EXCLUDE_FROM_ALL
     GIT_REPOSITORY https://github.com/google/zimtohrli.git
-    GIT_TAG v0.1.4
+    GIT_TAG v0.1.6
 )
 set(BUILD_ZIMTOHRLI_TESTS OFF CACHE INTERNAL "")
 FetchContent_MakeAvailable(zimtohrli)
